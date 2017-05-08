a07205763f90b9ad3562ef20061823f393eec95e
ashhab7 <m.ashhab614@gmail.com>
2017-05-08 10:21:22 +0600

    init push

--- demo.php
+++ demo.php
@@ -1,46 +1,33 @@
-<html>
+
 <?php
-session_start();
 /**
  * Created by PhpStorm.
  * User: DELL
- * Date: 3/28/2017
- * Time: 11:01
+ * Date: 4/25/2017
+ * Time: 19:18
  */
+
+session_start();
+
+
 include 'database.php';
 
-$emaillerr = $passerr = "";
+
 $email = $_SESSION["Email"];
 $pass = $_SESSION["pass"];
 
 
-echo $email;
-echo $pass;
+$account=$_SESSION['Account_Number'];
 
-if($conn->connect_error)
+$sql ="SELECT * FROM `user` WHERE Email = '$email'";
+$result = $conn->query($sql);
+$found=array();
+while($row = $result->fetch_assoc())
 {
-    echo "Connection not successful\n" . "<br>";
-    die("Connection failed: " .$conn->connect_error);
+    $found[]=$row;
 }
-//echo "Connect successfully" . "<br>";
 
-//$sql = sprintf('SELECT from user WHERE (Email = "%s" AND Password = "%s");',$email,$pass);
-    $sql = "SELECT * from user WHERE (Email = '$email' AND Password = $pass) ";
-    //$sql = "SELECT * FROM user";
-    $result = $conn->query($sql);
-        echo  "<br>" . "Log in successful";
-
-        while($row = $result->fetch_assoc())
-        {
-            echo "<br>" . "<b>User name: </b>" . $row["User_Name"] . " <b>Email Id: </b>" . $row["Email"]." <b>Password: </b>" . $row["Password"]." <b>Account Number: </b>" . $row["Account_Number"];
-        }
+echo json_encode($found);
 
 
-
-//$conn->close();
 ?>
-
-<body onload="window.location = 'customer.php'";>
-</body>
-
-</html>
